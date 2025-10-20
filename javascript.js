c44ece5ee4223f3357fb4e44abf41385989addfb
SalaccI4 <66245600+SalaccI4@users.noreply.github.com>
2025-10-19 19:24:27 -0700

    Implement change read status for any book on demand

--- javascript.js
+++ javascript.js
@@ -26,8 +26,17 @@ submitButton.addEventListener("click", (e) => {
     if (form.checkValidity()){
         e.preventDefault()
         dialog.close()
-        const finalBook = new Book(titleInput.value, authorInput.value, pagesInput.value, readInput.checked)
+        let read = ""
+        if(readInput.checked){
+            read = "Read"
+        }
+        else if(!readInput.checked){
+            read = "Not Read"
+        }
+        console.log(read)
+        const finalBook = new Book(titleInput.value, authorInput.value, pagesInput.value, read)
         finalBook.addBooktoLibrary(finalBook)
+        finalBook.addBooktoTable(finalBook)
         console.log(library)
         form.reset()
     }
@@ -46,20 +55,40 @@ Book.prototype.addBooktoLibrary = function(newBook){
 }
 
 Book.prototype.addBooktoTable = function(newBook) {
+    const readButton = document.createElement("button")
+    const delButton = document.createElement("button")
+    
     const tableRow = tableBody.insertRow(-1)
     const firstCell = tableRow.insertCell(0)
     const secondCell = tableRow.insertCell(1)
     const thirdCell = tableRow.insertCell(2)
     const fourthCell = tableRow.insertCell(3)
+    const fifthCell = tableRow.insertCell(4)
+    const sixthCell = tableRow.insertCell(5)
+
+    readButton.addEventListener("click", () => {
+        if (newBook.read == "Read"){
+            newBook.read = "Not Read"
+            fourthCell.innerHTML = newBook.read
+        }
+        else if (newBook.read == "Not Read"){
+            newBook.read = "Read"
+            fourthCell.innerHTML = newBook.read
+        }
+    })
 
     firstCell.innerHTML = newBook.title
     secondCell.innerHTML = newBook.author
     thirdCell.innerHTML = newBook.pages
     fourthCell.innerHTML = newBook.read
+    fifthCell.append(readButton)
+    readButton.innerHTML = "Change"
+    sixthCell.append(delButton)
+    delButton.innerHTML = "Delete"
 }
 
-const book1 = new Book("Harry Potter", "JK Rowling", 220, false)
-const book2 = new Book("Black Swan", "Your Mother", 254, true)
+const book1 = new Book("Harry Potter", "JK Rowling", 220, "Not Read")
+const book2 = new Book("Black Swan", "Your Mother", 254, "Read")
 book1.addBooktoLibrary(book1);
 book2.addBooktoLibrary(book2);
 book1.addBooktoTable(book1);
